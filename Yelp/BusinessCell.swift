375752a75172e9e53e7c511e5c603c62d9dfe6da
mirandamon <nmiranda4@fordham.edu>
2016-02-14 01:55:38 -0500

    added real info to UI

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,46 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Nathan Miranda on 2/13/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var reviewsCountLabel: UILabel!
+    @IBOutlet weak var categoriesLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var business: Business! {
+        didSet {
+            nameLabel.text = business.name
+            thumbImageView.setImageWithURL(business.imageURL!)
+            categoriesLabel.text = business.categories
+            addressLabel.text = business.address
+            reviewsCountLabel.text = "\(business.reviewCount!) Reviews"
+            ratingImageView.setImageWithURL(business.ratingImageURL!)
+            distanceLabel.text = business.distance
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        thumbImageView.layer.cornerRadius = 5
+        thumbImageView.clipsToBounds = true
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
